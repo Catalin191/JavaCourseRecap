955e9393419aa1cdc0d7052f067bdb497a6b4218
Catalin191 <62592391+Catalin191@users.noreply.github.com>
2022-01-31 22:27:05 +0200

    A good day of work.

---
+++ src/javaFundamentals/exObjectOrientedProgramming/ex1/Main.java
@@ -0,0 +1,8 @@
+package javaFundamentals.exObjectOrientedProgramming.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Dino", 14, "red");
+        dog1.bark();
+    }
+}
